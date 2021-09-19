fc29b84a0c198fd1da28da43d7f7460a809c4b45
Christopher Hadrian Wong <65900990+chrishadrian@users.noreply.github.com>
2021-09-18 20:00:04 -0700

    Set an alert when minute reaches 20

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,4 +1,4 @@
-const startingMinutes = 0;
+const startingMinutes = 19.95;
 let time = startingMinutes * 60;
 
 const countdownEl = document.getElementById('stopwatch');
@@ -8,10 +8,14 @@ setInterval(updateCountdown, 1000);
 function updateCountdown() {
     const minutes = Math.floor(time / 60);
     let seconds = time % 60;;
-
+    
     seconds = seconds <  10 ? '0' + seconds : seconds;
     
     countdownEl.innerHTML = `${minutes} : ${seconds}`;
+    if (minutes === 20) {
+        alert("Drink your water");
+        time = 0;
+    }
     time++;
 }
 
